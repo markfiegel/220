0f70e5aa840ac646a03389b2cb43d9e97353c3d8
markfiegel <markfiegel1@gmail.com>
2022-01-26 16:18:53 -0500

    completed lab2

---
+++ labs/lab2/lab2.py
@@ -0,0 +1,28 @@
+"""
+Mark Fiegel
+lab2.py
+I made a program that calculates 3 different formulas with inputted data
+I certify that this assignment is entirely my own work.
+"""
+import math
+def means():
+    range_values = eval(input("How many inputs do you have? "))
+    rms_accum = 0
+    harm = 0
+    geo = 1
+    for i in range(range_values):
+        x = eval(input("value? "))
+        rms_accum = rms_accum + x**2
+        harm = harm + 1/x
+        geo = geo * x
+
+    rms = (rms_accum/range_values) ** (1/2)
+    rms = round(rms,3)
+    harmonic_form = (range_values/harm)
+    harmonic_form = round(harmonic_form,3)
+    geometric_mean = (geo)**(1/range_values)
+    geometric_mean = round(geometric_mean,3)
+    print(rms)
+    print(harmonic_form)
+    print(geometric_mean)
+means()
