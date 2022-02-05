598e12b1b7de20e3a26ea91054bb3aad86bd26b3
markfiegel <markfiegel1@gmail.com>
2022-02-04 23:40:27 -0500

    completed lab3

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -1,35 +1,47 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
-
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
-Certification of Authenticity:
-<include one of the following>
+Mark Fiegel
+hw3.py
+I was given a series of problems that I created simple programs for
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
-
-
+import math
 def average():
-    pass
+    average_accum = 0
+    grades = eval(input("how many grades?  "))
+    for i in range(0,grades):
+        all_grades = eval(input("what's the grade?  "))
+        average_accum = average_accum + all_grades
+    print("average is" , average_accum/grades)
+
 
 
 def tip_jar():
-    pass
-
+    tip_accum = 0
+    for i in range(5):
+        tips = eval(input("How much would you like to donate?  "))
+        tip_accum = tip_accum + tips
+    print("total tips  " , tip_accum)
 
 def newton():
-    pass
-
-
+    x_newton = eval(input("x?"))
+    approx = eval(input("approximation?"))
+    x_val = 1
+    for i in range(1,approx+1):
+        next_x = (x_val+(x_newton/x_val))*.5
+        x_val = next_x
+    print(next_x)
 def sequence():
-    pass
-
-
+    terms = eval(input("how many terms are there?"))
+    for i in range (terms +1):
+        print(i)
 def pi():
-    pass
-
-
-if __name__ == '__main__':
-    pass
+    accum = 2
+    accum_2 = 1
+    approx = eval(input("how many terms in the series?"))
+    for i in range(1, approx+1):
+        first = (2*i)/((2*i)+1)
+        second = (2*i)/((2*i)-1)
+        accum = first * second
+        accum_pi = accum * accum_2
+    print(accum_pi*math.pi)
+pi()
