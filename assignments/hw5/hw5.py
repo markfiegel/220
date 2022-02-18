6c3dcddc14b9b039ab642583a34828d3ce0f2ac9
markfiegel <markfiegel1@gmail.com>
2022-02-18 12:51:18 -0500

    lab5

--- assignments/hw5/hw5.py
+++ assignments/hw5/hw5.py
@@ -1,50 +1,60 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Mark Fiegel
+hw5.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
-
+I was given a series of challenges with strings
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
-
-
 def name_reverse():
-    pass
-
-
+    namer = input("what is your first and last name? ")
+    name = namer.split(" ")
+    name.append(",")
+    print(name[1]+name[2], name[0])
 def company_name():
-    pass
-
+    domain = input("what is the domain")
+    domain = domain.split(".")
+    print(domain[1])
 
 def initials():
-    pass
-
-
+    students = eval(input("how many students in the class? "))
+    for _ in range(1,students+1):
+        mylist = input("what is the name? ")
+        mylist = mylist.split()
+        final =mylist[0][0]+mylist[1][0]
+        print(final)
 def names():
-    pass
+    namer = input("what are the names? ")
+    namer.upper()
+    names = namer.split(" ")
 
-
+    names_count = len(names)
+    for i in range(0,names_count-1,2):
+        namess = names[i][0]+names[i+1][0]
+        namess.title()
+        print(namess, end=" ")
 def thirds():
-    pass
+    sentence_num = eval(input("how many sentences?"))
+    x_val = []
+    for i in range(1, sentence_num+1):
 
-
+        print("enter sentence", i)
+        y_val = input("")
+        x_val.append(y_val[0::3])
+    for j in range(sentence_num):
+        print(x_val[j])
+thirds()
 def word_average():
-    pass
-
-
+    sentence = input("enter a sentence: ")
+    sentences = sentence.split()
+    sentence_len = len(sentences)
+    x_val = 0
+    for i in range(sentence_len):
+        y_val = len(sentences[i])
+        x_val = x_val+y_val
+    print(x_val/sentence_len)
 def pig_latin():
-    pass
-
-
-if __name__ == '__main__':
-    # name_reverse()
-    # company_name()
-    # initials()
-    # names()
-    # thirds()
-    # word_average()
-    # pig_latin()
-    pass
+    words = input("enter a sentence to convert to pig latin: ").split()
+    for word in words:
+        wordes = word[1:] + word[0]
+        print(wordes.lower() + "ay", end=" ")
